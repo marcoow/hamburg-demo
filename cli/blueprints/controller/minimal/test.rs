ed5eb824cd1ab9571f8294911088cc4eecc85c32
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-11-28 12:39:01 +0100

    initial

---
+++ cli/blueprints/controller/minimal/test.rs
@@ -0,0 +1,31 @@
+{%- if has_db -%}
+use crate::test_helpers::{BodyExt, DbTestContext, RouterExt};
+{% else %}
+use crate::test_helpers::{BodyExt, TestContext, RouterExt};
+{%- endif -%}
+use axum::{body::Body, http::Method};
+use googletest::prelude::*;
+use hyper::StatusCode;
+use {{macros_crate_name}}::test;
+use std::collections::HashMap;
+
+use crate::common;
+
+#[db_test]
+{%- if has_db %}
+async fn test_action(context: &DbTestContext) {
+{%- else -%}
+async fn test_action(context: &TestContext) {
+{% endif %}
+    todo!("implement and assert on status code!");
+
+    /* Example:
+    let response = context
+        .app
+        .request("/{{name}}/action")
+        .send()
+        .await;
+
+    assert_that!(response.status(), eq(StatusCode::OK));
+    */
+}
